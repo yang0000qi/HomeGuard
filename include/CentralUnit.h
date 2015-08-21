8ea10aa03813ae314617e1e0e8e67706573008f0
chenbin <heychenbin@gmail.com>
2015-08-22 01:23:40 +0800

    typo fix for CentralUnit

--- include/CentralUnit.h
+++ include/CentralUnit.h
@@ -1,55 +1,53 @@
-﻿
-#pragma once
+﻿#pragma once
 
-#include <string>
-#include <memory>
-#include <vector>
+
 #include <map>
-
-class Sensor;
+#include <memory>
+#include <string>
+#include <vector>
 
 #include "AudibleAlarm.h"
 #include "HomeguardView.h"
 
 
-class CentralUnit
-{
+class Sensor;
+
+class CentralUnit {
 public:
+    static std::string PASS;
+    static std::string FAIL;
+    static std::string PENDING;
+    static std::string READY;
 
-	static std::string PASS;
-	static std::string FAIL;
-	static std::string PENDING;
-	static std::string READY;
+    CentralUnit();
 
-	CentralUnit();
+    std::string getSensorMessage(const Sensor& sensor) const;
+    bool isArmed() const;
+    void arm();
+    void disarm();
 
-	std::string getSensorMessage(const Sensor& sensor) const;
-	bool isArmed() const;
-	void arm();
-	void disarm();
-	
-	bool isValidCode(const std::string& code) const;
-	void setSecurityCode(const std::string& code);
-	void enterCode(const std::string& code);
+    bool isValidCode(const std::string& code) const;
+    void setSecurityCode(const std::string& code);
+    void enterCode(const std::string& code);
 
-	std::vector<Sensor>& getSensors();
-	std::map<std::string,std::string>& getSensorStatusMap();
-	void registerSensor(const Sensor& sensor);
-	void parseRadioBroadcast(const std::string& packet);
-	void runSensorTest();
-	Sensor getSensor(const std::string& id) const;
-	std::string getSensorStatus() const;
-
-private:
-	void terminateSensorTest();
+    std::vector<Sensor>& getSensors();
+    std::map<std::string,std::string>& getSensorStatusMap();
+    void registerSensor(const Sensor& sensor);
+    void parseRadioBroadcast(const std::string& packet);
+    void runSensorTest();
+    Sensor getSensor(const std::string& id) const;
+    std::string getSensorStatus() const;
 
 private:
-	std::string sensorTestStatus;
-	bool armed;
-	std::string securityCode;
-	std::auto_ptr<AudibleAlarm> audibleAlarm;
-	std::auto_ptr<HomeguardView> view;
-	std::vector<Sensor> sensors;
-	bool runningSensorTest;
-	std::map<std::string,std::string> sensorStatusMap;
+    void terminateSensorTest();
+
+private:
+    std::string sensorTestStatus;
+    bool armed;
+    std::string securityCode;
+    std::auto_ptr<AudibleAlarm> audibleAlarm;
+    std::auto_ptr<HomeguardView> view;
+    std::vector<Sensor> sensors;
+    bool runningSensorTest;
+    std::map<std::string,std::string> sensorStatusMap;
 };
