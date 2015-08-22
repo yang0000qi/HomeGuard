dd8640a8e9bbff113edd73e20f9c6cffb83bb8f6
chenbin <heychenbin@gmail.com>
2015-08-22 14:28:26 +0800

    增加查询sensor的用例

--- test/testCentralUnit.cpp
+++ test/testCentralUnit.cpp
@@ -12,3 +12,23 @@ TEST_CASE("test parse packet", "[CentralUnit,unit]") {
         CHECK(status == "TRIPPED");
     }
 }
+
+TEST_CASE("test find sensor by id", "[CentralUnit,unit]") {
+    CentralUnit cu;
+    Sensor s1("1", "door", Sensor::Type::DOOR);
+    Sensor s2("2", "window", Sensor::Type::FIRE);
+    Sensor s3("3", "cell", Sensor::Type::WINDOW);
+    cu.registerSensor(s1);
+    cu.registerSensor(s2);
+    cu.registerSensor(s3);
+
+    Sensor s = cu.getSensor("1");
+    CHECK(s.id == "1");
+    CHECK(s.location == "door");
+    CHECK(s.type == Sensor::Type::DOOR);
+
+    s = cu.getSensor("3");
+    CHECK(s.id == "3");
+    CHECK(s.location == "cell");
+    CHECK(s.type == Sensor::Type::WINDOW);
+}
