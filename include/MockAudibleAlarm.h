b2cd052939bf58962b5fa9eda09b679f5157c7f1
chenbin <chen.bin11@zte.com.cn>
2015-08-24 12:57:54 +0800

    通过MockAlarm测试alarm的有效性

--- include/MockAudibleAlarm.h
+++ include/MockAudibleAlarm.h
@@ -1,9 +1,14 @@
 ﻿#pragma once
 
+#include "AudibleAlarm.h"
+
 
 class MockAudibleAlarm : public AudibleAlarm
 {
 public:
-    virtual void sound();
-    virtual void silence();
+    virtual void sound() { alarmed = true; }
+    virtual void silence() { alarmed = false; }
+
+public:
+    bool alarmed = false;
 };
--- test/testSecurityPanel.cpp
+++ test/testSecurityPanel.cpp
@@ -1,18 +1,25 @@
 #include "catch.hpp"
 #define private public
+#include "MockAudibleAlarm.h"
 #include "SecurityPanel.h"
 
 
 TEST_CASE("test security panel", "[SecurityPanel,unit]") {
     SecurityPanel sp;
+    sp._audibleAlarm.reset((AudibleAlarm *)(new MockAudibleAlarm));
+    std::shared_ptr<MockAudibleAlarm> mockAlarm =
+        std::static_pointer_cast<MockAudibleAlarm>(sp._audibleAlarm);
     sp.setSecurityCode("this is correct password");
     CHECK(sp.isValidCode("this is not correct password") == false);
     CHECK(sp.isValidCode("this is correct password") == true);
 
     sp.arm();
     CHECK(sp.isArmed() == true);
+    sp.alarm();
+    CHECK(mockAlarm->alarmed == true);
     sp.enterCode("this is not correct password");
     CHECK(sp.isArmed() == true);
     sp.enterCode("this is correct password");
     CHECK(sp.isArmed() == false);
+    CHECK(mockAlarm->alarmed == false);
 }
